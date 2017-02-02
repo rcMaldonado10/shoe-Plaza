2e90b594b1e8475cffdadbbea39ea36fa508ade8
xlKennethlx <kenneth.velez2@upr.edu>
2017-02-01 21:34:59 -0400

    Dam
    Ahora si el sing up esta de puta madre

--- singUpPage.php
+++ singUpPage.php
@@ -1,9 +1,25 @@
-<link rel="stylesheet" type="text/css" href="creditCardStyle.css"/>
-<div class="form">
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sign-Up/Login Form</title>
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+
+      <link rel="stylesheet" href="signUpStyle.css">
+
+
+</head>
+
+<body>
+  <div class="form">
+
       <ul class="tab-group">
         <li class="tab active"><a href="#signup">Sign Up</a></li>
         <li class="tab"><a href="#login">Log In</a></li>
       </ul>
+
       <div class="tab-content">
         <div id="signup">
           <h1>Sign Up for Free</h1>
@@ -76,3 +92,9 @@
       </div><!-- tab-content -->
 
 </div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="chuleria.js"></script>
+
+</body>
+</html>
