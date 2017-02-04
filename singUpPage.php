66adf3dbf1a2909f9391f64d6283cfa81676247c
yatio <yadiel.nieves1@upr.edu>
2017-02-03 20:56:08 -0400

    Ya se acabo
    ya se acabo creo xD

--- singUpPage.php
+++ singUpPage.php
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-
-
-<div class="form">
-=======
 <!DOCTYPE html>
 <html >
 <head>
@@ -21,7 +15,6 @@
 <body>
   <div class="form">
 
->>>>>>> origin/master
       <ul class="tab-group">
         <li class="tab active"><a href="#signup">Sign Up</a></li>
         <li class="tab"><a href="#login">Log In</a></li>
@@ -35,39 +28,60 @@
 
           <div class="top-row">
             <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" />
-            </div>
+               <label>
+                 First Name<span class="req">*</span>
+               </label>
+              <input type="text" required autocomplete="off" name="firstName" />
+             </div>
 
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off"/>
-            </div>
-          </div>
+             <div class="field-wrap">
+               <label>
+                 Last Name<span class="req">*</span>
+               </label>
+             <input type="text"required autocomplete="off" name="lastName"/>
+             </div>
+           </div>
 
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off"/>
-          </div>
+           <div class="field-wrap">
+             <label>
+               Email Address<span class="req">*</span>
+             </label>
+            <input type="email"required autocomplete="off" name="email"/>
+           </div>
 
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off"/>
-          </div>
+           <div class="field-wrap">
+             <label>
+               Set A Password<span class="req">*</span>
+             </label>
 
-          <button type="submit" class="button button-block"/>Get Started</button>
+            <input type="password"required autocomplete="off" name="password"/>
+           </div>
+            <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Login">          <!--<button onclick="myChange()">Copy Text</button> -->
+          <!--<button type="submit" class="button button-block"/>Get Started</button> -->
 
-          </form>
+<?php
+function myChange(){
+$servername = "localhost";
+$username = "Yatio";
+$password = "mayo1996";
+$Table = "testdb1";
 
-        </div>
+$dateBase = new mysqli($servername,$username,$password,$Table) or die("Unable to connect");
+
+$firstName = $_POST['firstName'];
+echo firstName;
+
+$sql = "INSERT INTO table1 (Nombre,Apellido,numero) VALUES('Hola','idk','16')";
+$result = mysqli_query($dateBase, $sql) or die("Bad query: $sql");
+echo "Good Query";
+}
+
+
+
+ ?>
+           </form>
+
+         </div>
 
         <div id="login">
           <h1>Welcome Back!</h1>
@@ -99,18 +113,9 @@
       </div><!-- tab-content -->
 
 </div> <!-- /form -->
-<<<<<<< HEAD
-
-
-
-
-
-
-
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
     <script src="chuleria.js"></script>
 
 </body>
 </html>
->>>>>>> origin/master
