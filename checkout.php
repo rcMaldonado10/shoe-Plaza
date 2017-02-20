111e20409e96d85de4d71f6acd2718af40f4a3a2
xlKennethlx <kenneth.velez2@upr.edu>
2017-02-20 14:47:04 -0400

    cart button

--- checkout.php
+++ checkout.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" type="text/css" href="stylesstore.css"/>
 </head>
 <body>
-
     <div id="container">
         <div id="topbar">
 
@@ -55,6 +54,19 @@
                     Name: Rafael E. Colon Maldonado</br>
                     Shipping Address: Barceloneta, Puerto Rico 00617</br>
                     Credit Card: XXXX-XXXX-XXXX-1234</br>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+        </button>
+      </p>
+        <a href="#" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart
+        </a>
+      </p>
+    </div>
+  </body>
                 </fieldset>
             </form>
 
