1c283986e0892c164478fbe0cd6643d51227c3b7
MonsterGonzalez <gabrielgonzalez@Gabriels-MacBook-Pro.local>
2017-02-23 12:36:58 -0400

    pepe

--- checkout.php
+++ checkout.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 
-   <div id="container">
+<div id="container">
         <div id="topbar">
 
                 <div class="fixedwith">
@@ -26,8 +26,8 @@
             <div id="topmenudiv">
                 <ul>
                     <li class="default"><a href="home.php">Home</a></li>
-                    <li class="default"><a href= "women.php">Women </a></li>
-                    <li class = "default"><a href="men.php">Men</a></li>
+                    <li class="default"><a href="women.php">Women </a></li>
+                    <li class="default"><a href="men.php">Men</a></li>
                     <li class="default"><a href="about.php">About us</a></li>
 
                 </ul>
@@ -35,26 +35,21 @@
 
             </div>
                 <div class="break" ></div>
-
-                    <div id="newsbar">
+                    
                         <div class ="fixedwith">
 
                             <p id="titleheader" style="color: black;">Checkout</p>
 
-                             <div class="break" ></div>
-
-                        </div>
-
                  </div>
-
-            <div class="break" ></div>
-
-            <form action="">
+                 <div class="break" ></div>
+                 
+                 <form action="">
                 <fieldset style="color: black;">
                     Name: Rafael E. Colon Maldonado</br>
                     Shipping Address: Barceloneta, Puerto Rico 00617</br>
                     Credit Card: XXXX-XXXX-XXXX-1234</br>
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    </fieldset>
+                     </form>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -65,10 +60,13 @@
           <span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart
         </a>
       </p>
-    </div>
+   
   
-                </fieldset>
-            </form>
+                
+           
 
+                 
+    </div>
+</div>
 </body>
 </html>
