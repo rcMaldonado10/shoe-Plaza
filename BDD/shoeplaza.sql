261015ddaa73d0732a07e717f149bf1be19a6d4c
yatio <yadiel.nieves1@upr.edu>
2017-04-03 12:24:17 -0400

    k again
    well quisas

--- BDD/shoeplaza.sql
+++ BDD/shoeplaza.sql
@@ -14,14 +14,8 @@ CREATE TABLE `customer` (
   `FirstName` varchar(25) COLLATE utf16_unicode_520_ci NOT NULL,
   `LastName` varchar(25) COLLATE utf16_unicode_520_ci NOT NULL,
   `Password` varchar(25) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Shipping_State` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Shipping_City` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Shipping_Postal_Add` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Shipping_Street_Add` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Billing_State` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Billing_City` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Billing_Postal_Add` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
-  `Billing_Street_Add` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
+  `Shipping_Address` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
+  `Billing_Address` varchar(255) COLLATE utf16_unicode_520_ci NOT NULL,
   `Status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_520_ci;
 
@@ -32,7 +26,6 @@ CREATE TABLE `customer` (
 --
 
 CREATE TABLE `customer_credit_card` (
-  `CustomerID` int(10) NOT NULL,
   `Credit_Card_ID` int(10) NOT NULL,
   `Number` int(25) NOT NULL,
   `Name` int(75) NOT NULL,
@@ -50,7 +43,7 @@ CREATE TABLE `has` (
   `CustomerID` int(10) NOT NULL,
   `OrderID` int(10) NOT NULL,
   `DateOrderMade` date NOT NULL,
-  `Credit_CardID` int(10) NOT NULL
+  `Credit_Payment` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_520_ci;
 
 -- --------------------------------------------------------
@@ -85,7 +78,7 @@ CREATE TABLE `is_in` (
 
 CREATE TABLE `makes` (
   `CompanyID` int(10) NOT NULL,
-  `CustomerID` int(11) NOT NULL
+  `Tracking_Number` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_520_ci;
 
 -- --------------------------------------------------------
@@ -132,8 +125,6 @@ CREATE TABLE `shipper` (
 --
 
 CREATE TABLE `shipping` (
-  `CustomerID` int(10) NOT NULL,
-  `OrderID` int(10) NOT NULL,
   `Shipping_Status` tinyint(1) NOT NULL,
   `Tracking_Number` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_520_ci;
@@ -180,32 +171,62 @@ ALTER TABLE `customer_credit_card`
 -- Indexes for table `has`
 --
 ALTER TABLE `has`
-  ADD PRIMARY KEY (`CustomerID`,`OrderID`);
+  ADD PRIMARY KEY (`CustomerID`,`OrderID`),
+  ADD KEY `OrderID` (`OrderID`);
 
 --
 -- Indexes for table `has_a`
 --
 ALTER TABLE `has_a`
-  ADD PRIMARY KEY (`CustomerID`,`Credit_Card_ID`);
+  ADD PRIMARY KEY (`CustomerID`,`Credit_Card_ID`),
+  ADD KEY `Credit_Card_ID` (`Credit_Card_ID`);
 
 --
 -- Indexes for table `is_in`
 --
 ALTER TABLE `is_in`
-  ADD PRIMARY KEY (`OrderID`,`ProductID`);
+  ADD PRIMARY KEY (`OrderID`,`ProductID`),
+  ADD KEY `ProductID` (`ProductID`);
 
 --
 -- Indexes for table `makes`
 --
 ALTER TABLE `makes`
-  ADD PRIMARY KEY (`CompanyID`,`CustomerID`);
+  ADD PRIMARY KEY (`CompanyID`,`Tracking_Number`),
+  ADD KEY `Tracking_Number` (`Tracking_Number`);
 
 --
 -- Indexes for table `order_`
 --
 ALTER TABLE `order_`
   ADD PRIMARY KEY (`OrderID`,`CustomerID`,`ProductID`),
-  ADD KEY `CustomerID` (`CustomerID`);
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indexes for table `shipped_by`
+--
+ALTER TABLE `shipped_by`
+  ADD PRIMARY KEY (`OrderID`,`CompanyID`),
+  ADD KEY `CompanyID` (`CompanyID`);
+
+--
+-- Indexes for table `shipper`
+--
+ALTER TABLE `shipper`
+  ADD PRIMARY KEY (`CompanyID`);
+
+--
+-- Indexes for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD PRIMARY KEY (`Tracking_Number`);
+
+--
+-- Indexes for table `shoe`
+--
+ALTER TABLE `shoe`
+  ADD PRIMARY KEY (`ProductID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -226,6 +247,45 @@ ALTER TABLE `customer_credit_card`
 --
 ALTER TABLE `order_`
   MODIFY `OrderID` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `has`
+--
+ALTER TABLE `has`
+  ADD CONSTRAINT `has_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `has_ibfk_2` FOREIGN KEY (`OrderID`) REFERENCES `order_` (`OrderID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `has_a`
+--
+ALTER TABLE `has_a`
+  ADD CONSTRAINT `has_a_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `has_a_ibfk_2` FOREIGN KEY (`Credit_Card_ID`) REFERENCES `customer_credit_card` (`Credit_Card_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `is_in`
+--
+ALTER TABLE `is_in`
+  ADD CONSTRAINT `is_in_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `order_` (`OrderID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `is_in_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `shoe` (`ProductID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `makes`
+--
+ALTER TABLE `makes`
+  ADD CONSTRAINT `makes_ibfk_1` FOREIGN KEY (`CompanyID`) REFERENCES `shipper` (`CompanyID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `makes_ibfk_2` FOREIGN KEY (`Tracking_Number`) REFERENCES `shipping` (`Tracking_Number`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `shipped_by`
+--
+ALTER TABLE `shipped_by`
+  ADD CONSTRAINT `shipped_by_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `order_` (`OrderID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `shipped_by_ibfk_2` FOREIGN KEY (`CompanyID`) REFERENCES `shipper` (`CompanyID`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
